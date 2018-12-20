d78f28f339e53a862cf957ed293d22cdbf126989
Alexander Souza <alexpt2000@gmail.com>
2018-12-20 22:17:32 +0000

    046. Servic  o HTTP para o Detalhe do Restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -14,6 +14,13 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
         .map(response => response.json())
-        .catch(ErrorHandler.handleError)
+        .catch(ErrorHandler.handleError);
     }
+
+    restaurantById(id: string): Observable<Restaurant>{
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+    }
+
 }
