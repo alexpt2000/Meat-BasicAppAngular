0f01ee54567e58f2ae552db37c148a93e8e10c2e
Alexander Souza <alexander.souza@gm.com>
2019-02-03 12:49:29 +0000

    067. Finalizando a Compra (HTTP POST)

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,30 +1,46 @@
 import { CartItem } from './../restaurant-detail/shopping-cart/cart-item.model';
 import { ShoppingCartService } from './../restaurant-detail/shopping-cart/shopping-cart.service';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import { MEAT_API } from 'app/app.api';
+import { Order } from './order.model';
 
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService) { }
-
+  constructor(private cartService: ShoppingCartService, private http: Http) { }
+  
   carteItems(): CartItem[] {
     return this.cartService.items;
   }
-
+  
   increaseQty(item: CartItem) {
     this.cartService.increaseQty(item);
   }
-
+  
   decreaseQty(item: CartItem) {
     this.cartService.decreaseQty(item);
   }
-
+  
   remove(item: CartItem) {
     this.cartService.removeItem(item);
   }
-
+  
   itemsValue(): number {
     return this.cartService.total();
+  }
+  
+  checkOrder(order: Order): Observable<string> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), 
+    new RequestOptions({headers: headers}))
+    .map(response=> response.json());
+  }
+
+  clear() {
+    this.cartService.clear();
   }
 
 }
