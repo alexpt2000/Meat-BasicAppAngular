d7ef2dd47d17f490b633c26d86fb0cb09f0c7acf
Alexander Souza <alexander.souza@gm.com>
2019-02-03 21:27:26 +0000

    071. Refatorando a Aplicac  a  o para Reactive Forms

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,8 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-
-
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -13,7 +12,9 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
-  delivery = 8;
+  orderForm: FormGroup;
+
+  delivery: number = 8;
 
   paymentOptions: RadioOption[] = [
     { label: 'Dinheiro', value: 'MON' },
@@ -21,9 +22,23 @@ export class OrderComponent implements OnInit {
     { label: 'Cartão de Refeição', value: 'REF' }
   ];
 
-  constructor(private orderService: OrderService, private router: Router) { }
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private formBuilder: FormBuilder)
+    { }
 
   ngOnInit() {
+
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control(''),
+      email: this.formBuilder.control(''),
+      emailConfirmation: this.formBuilder.control(''),
+      address: this.formBuilder.control(''),
+      number: this.formBuilder.control(''),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('')
+    });
   }
 
   itemsValue(): number {
