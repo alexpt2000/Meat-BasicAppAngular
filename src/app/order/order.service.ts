990c98d057cd021a33ccefd8a7b0a7a3375d6b2b
Alexander Souza <alexander.souza@gm.com>
2019-02-03 14:21:19 +0000

    068. Navegac  a  o Programa  tica via Router

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -10,33 +10,34 @@ import { Order } from './order.model';
 @Injectable()
 export class OrderService {
   constructor(private cartService: ShoppingCartService, private http: Http) { }
-  
+
   carteItems(): CartItem[] {
     return this.cartService.items;
   }
-  
+
   increaseQty(item: CartItem) {
     this.cartService.increaseQty(item);
   }
-  
+
   decreaseQty(item: CartItem) {
     this.cartService.decreaseQty(item);
   }
-  
+
   remove(item: CartItem) {
     this.cartService.removeItem(item);
   }
-  
+
   itemsValue(): number {
     return this.cartService.total();
   }
-  
+
   checkOrder(order: Order): Observable<string> {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order), 
-    new RequestOptions({headers: headers}))
-    .map(response=> response.json());
+    return this.http.post(`${MEAT_API}/orders`, JSON.stringify(order),
+      new RequestOptions({ headers: headers }))
+      .map(response => response.json())
+      .map(order => order.id);
   }
 
   clear() {
