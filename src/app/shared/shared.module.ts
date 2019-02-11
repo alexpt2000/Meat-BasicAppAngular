efa035db9b5890a0b96565abf4f0127fb35c596c
Alexander Souza <alexpt2000@gmail.com>
2019-02-11 18:55:28 +0000

    079. Adicionando Servicos a um Modulo Compartilhado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
 import { InputComponent } from './input/input.component';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { OrderService } from 'app/order/order.service';
 
 @NgModule({
   declarations: [InputComponent, RadioComponent, RatingComponent],
@@ -12,5 +15,10 @@ import { CommonModule } from '@angular/common';
 })
 
 export class SharedModule {
-
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    }
+  }
 }
