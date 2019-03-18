25fb47785c36586483bc7a24def482adaf71cb61
Alexander Souza <alexpt2000@gmail.com>
2019-03-18 20:48:26 +0000

    105. Prevenindo a Interrupcao do Observable ValueChanges

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -5,6 +5,9 @@ import { RestaurantsService } from './restaurants.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-restaurants',
@@ -45,7 +48,8 @@ export class RestaurantsComponent implements OnInit {
     .debounceTime(500)
     .distinctUntilChanged()
     .switchMap(searchTerm =>
-      this.restaurantsService.restaurants(searchTerm))
+      this.restaurantsService.restaurants(searchTerm)
+      .catch(error => Observable.from([])))
       .subscribe(restaurants => this.restaurants = restaurants);
 
     this.restaurantsService.restaurants()
